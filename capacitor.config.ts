da299c2bcc767818dfece6ad36c1e269500ee661
elomar8 <123784890+elomar8@users.noreply.github.com>
2023-02-01 23:47:37 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Practica-1-2023951',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
